7869c0289ff1303ec46c18a42bc953aca6d18514
Vishwajeet Sanjay Kadam <66294989+vishwajeet550@users.noreply.github.com>
2022-06-11 21:18:34 +0530

    Update aws-download.py

--- LY-upload-to-s3/aws-download.py
+++ LY-upload-to-s3/aws-download.py
@@ -2,8 +2,8 @@ import boto3
 import logging
 
 session = boto3.Session( 
-         aws_access_key_id='AKIAZJ4TGNJJQBTRNUSL', 
-         aws_secret_access_key='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')
+         aws_access_key_id='', 
+         aws_secret_access_key='')
 
 # Then use the session to get the resource
 s3 = session.resource('s3')
@@ -11,8 +11,8 @@ my_bucket = s3.Bucket('test-images-v')
 
 # Get the data from S3
 s3_client = boto3.client('s3', 
-            aws_access_key_id='AKIAZJ4TGNJJQBTRNUSL',
-            aws_secret_access_key='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',
+            aws_access_key_id='',
+            aws_secret_access_key='',
             region_name='ap-south-1')
 
 for my_bucket_object in my_bucket.objects.all():
