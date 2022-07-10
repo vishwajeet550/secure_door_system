5987fb486d810c36f4bd018939a3b895763e380d
Vishwajeet Sanjay Kadam <66294989+vishwajeet550@users.noreply.github.com>
2022-07-10 13:20:05 +0530

    Update aws-upload.php

--- LY-upload-to-s3/aws-upload.php
+++ LY-upload-to-s3/aws-upload.php
@@ -4,18 +4,4 @@ $command = escapeshellcmd('python aws-upload.py');
 $output = shell_exec($command);
 echo $output;
 
-// $command = escapeshellcmd('dir');
-// $output = shell_exec($command);
-// echo $output;
-
-// $r=shell_exec("python aws-upload.py"); 
-// echo $r;
-
-// echo '<pre>'; 
-// $last_line = system('aws-upload.py', $retval); 
-// echo ' 
-// </pre> 
-// <hr />Last line of the output: ' . $last_line . ' 
-// <hr />Return value: ' . $retval; 
-
 ?>
