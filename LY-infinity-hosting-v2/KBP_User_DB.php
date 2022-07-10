9260e265affc2ea43833cd547d3680b402989f6f
Vishwajeet Sanjay Kadam <66294989+vishwajeet550@users.noreply.github.com>
2022-07-10 13:18:52 +0530

    Update KBP_User_DB.php

--- LY-infinity-hosting-v2/KBP_User_DB.php
+++ LY-infinity-hosting-v2/KBP_User_DB.php
@@ -7,11 +7,6 @@ define('DB_USERNAME', 'root');
 define('DB_PASSWORD', '');
 define('DB_NAME', 'kbp2_users2');
 
-// define('DB_SERVER', 'sql111.epizy.com');
-// define('DB_USERNAME', 'epiz_30424908');
-// define('DB_PASSWORD', 'gtwED1WwFlM');
-// define('DB_NAME', 'epiz_30424908_kbp_cred');
-
 /* Attempt to connect to MySQL database */
 $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
  
